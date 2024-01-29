2d7ba2c07aaf2917bd3e29a460304f387fe0e8c3
MatDat <math03t7@stud.kea.dk>
2024-01-29 14:07:28 +0100

    Structure made

--- script.js
+++ script.js
@@ -5,3 +5,15 @@ window.addEventListener("load", start);
 function start() {
   console.log("Javascript is running!");
 }
+
+function generateRandomNumber() {}
+
+function receiveGuess() {}
+
+function evaluateGuess() {}
+
+function guessIsCorrect() {}
+
+function guessIsTooLow() {}
+
+function guessIsTooHigh() {}
